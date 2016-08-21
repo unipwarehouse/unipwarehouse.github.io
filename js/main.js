d37e8128769f71ca444e604e5f1c0cf33c01364e
Abdurrahman Al Hanif <abdurrahman.alhanif@gmail.com>
2016-08-21 13:03:12 +0700

    jekyll ready for deployment

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 // Affix for navbar)
 $(document).ready(function(){
 	var na  = $(".navbar-affix");
-			nas = "navbar-affix-scrolled";
-			hdr = $('header').height();
+	var nas = "navbar-affix-scrolled";
+	var hdr = $('header').outerHeight();
 
 	$(window).scroll(function() {
 	  if( $(this).scrollTop() > hdr - na.outerHeight()) {
-	    na.addClass(nas);
+	    $(na).addClass(nas);
 	  } else {
-	    na.removeClass(nas);
+	    $(na).removeClass(nas);
 	  }	
 	});
 });
